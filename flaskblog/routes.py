485e68f43f8eac41bf421e9a11398a1e1f73affb
Luis Kumruyan <nando@kyan.com.br>
2019-06-05 00:11:04 -0300

    Videos 4 and 5 of series

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'secretkey123'
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 posts = [
     {
@@ -47,6 +45,3 @@ def login():
         else:
             flash('Log in Unsuccessful! Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-if __name__ == "__main__":
-    app.run(debug=True)
